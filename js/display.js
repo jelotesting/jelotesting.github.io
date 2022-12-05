ed47199c4a0f64a9872b81cceb6beb29f633a147
jelotesting <118308520+jelotesting@users.noreply.github.com>
2022-12-05 11:14:30 +0300

    Add files via upload

--- js/display.js
+++ js/display.js
@@ -3,55 +3,45 @@ var emailArray=[];
 
         var loginBox = document.getElementById("login");
         var regBox = document.getElementById("register");
-        var forgetBox = document.getElementById("forgot");
+        // var forgetBox = document.getElementById("forgot");
 
         var loginTab = document.getElementById("lt");
         var regTab = document.getElementById("rt");
-        var endTab = document.getElementById("et");
+        // var endTab = document.getElementById("et");
 
         function regTabFun(){
             event.preventDefault();
 
             regBox.style.visibility="visible";
             loginBox.style.visibility="hidden";
-            forgetBox.style.visibility="hidden";
+            // forgetBox.style.visibility="hidden";
 
             regTab.style.backgroundColor="#ffffff";
             loginTab.style.backgroundColor="transparent";
-            endTab.style.backgroundColor="transparent";
+            // endTab.style.backgroundColor="transparent";
         }
         function loginTabFun(){
             event.preventDefault();
 
             regBox.style.visibility="hidden";
             loginBox.style.visibility="visible";
-            forgetBox.style.visibility="hidden";
+            // forgetBox.style.visibility="hidden";
 
             loginTab.style.backgroundColor="#ffffff";
             regTab.style.backgroundColor="transparent";
-            endTab.style.backgroundColor="transparent";
+            // endTab.style.backgroundColor="transparent";
         }
-        function endTabFun(){
-            event.preventDefault();
-
-            regBox.style.visibility="hidden";
-            loginBox.style.visibility="hidden";
-            forgetBox.style.visibility="visible";
-
-            endTab.style.backgroundColor="#ffffff";
-            regTab.style.backgroundColor="transparent";
-            loginTab.style.backgroundColor="transparent";
-        }
+        
         function forTabFun(){
             event.preventDefault();
 
             regBox.style.visibility="hidden";
             loginBox.style.visibility="hidden";
-            forgetBox.style.visibility="hidden";
+            // forgetBox.style.visibility="hidden";
 
             regTab.style.backgroundColor="transparent";
             loginTab.style.backgroundColor="transparent";
-            endTab.style.backgroundColor="transparent";
+            // endTab.style.backgroundColor="transparent";
 
         }
 
@@ -127,20 +117,20 @@ var emailArray=[];
             }
 
         }
-        function forgot(){
-            event.preventDefault();
+        // function forgot(){
+        //     event.preventDefault();
 
-            var email = document.getElementById("fe").value;
+        //     var email = document.getElementById("fe").value;
 
-            if(emailArray.indexOf(email) == -1){
-                if (email == ""){
-                    alert("Email required.");
-                    return ;
-                }
-                alert("Email does not exist.");
-                return ;
-            }
+        //     if(emailArray.indexOf(email) == -1){
+        //         if (email == ""){
+        //             alert("Email required.");
+        //             return ;
+        //         }
+        //         alert("Email does not exist.");
+        //         return ;
+        //     }
 
-            alert("email is send to your email check it in 24hr. \n Thanks");
-            document.getElementById("fe").value ="";
-        }
+        //     alert("email is send to your email check it in 24hr. \n Thanks");
+        //     document.getElementById("fe").value ="";
+        // }
